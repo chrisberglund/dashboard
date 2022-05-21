4529b8766e6b4634cfd68f561fb0df368fa24b53
Christopher Berglund <13christopherb@gmail.com>
2022-05-21 15:05:48 -0700

    First plot for Rshiny dashboard

--- app.R
+++ app.R
@@ -1,24 +1,21 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("NFL Winning percentage"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+          sliderInput("minutes",
+                      "Minute",
+                      min = 0,
+                      max = 70,
+                      value = 1)
         ),
 
         # Show a plot of the generated distribution
@@ -30,12 +27,17 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
     df <- read.csv("~/Desktop/football_stats.csv")
-    game <- df[which(df$ID == 401326316),]
+    df$Minute <- floor(df$Time / 60)
     
     output$gamePlot <- renderPlot({
-        ggplot(game, aes(x=Time, y=Percent)) + geom_line()
+      df <- df[which(df$Minute == input$minutes), ]
+      df$Percent <- round(df$Percent, 1)
+      df$Correct <- as.integer((df$Percent > 0.5 & df$Home_Win))
+      correct_prcnt <- aggregate(df$Home_Win, by=list(df$Percent), FUN=sum)
+      correct_count <- df %>% count(Percent)
+      correct_prcnt$x <- correct_prcnt$x / correct_count$n
+      ggplot(correct_prcnt, aes(x=Group.1, y=x)) + geom_line() + labs(x = "ESPN winning percentage", y = "Actual winning percentage")
     })
 }
 
