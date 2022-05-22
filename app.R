dec10731a0a70604c91f23b1d9cef266caa25996
Christopher Berglund <13christopherb@gmail.com>
2022-05-21 21:57:51 -0700

    Flacco

--- app.R
+++ app.R
@@ -16,6 +16,7 @@ ui <- fluidPage(
         ),
 
         mainPanel(
+          plotOutput("flaccoMeter"),
            plotOutput("gamePlot")
         )
     )
@@ -24,6 +25,23 @@ ui <- fluidPage(
 server <- function(input, output) {
     df <- read.csv("~/Desktop/football_stats.csv")
     df$Minute <- floor(df$Time / 60)
+    
+    flacco_search <- read.csv("~/Desktop/joe_flacco.csv")
+    flac_stats <- read.csv("~/Desktop/flacco_stats.csv")
+    flac_stats <- flac_stats[which(!is.na(as.integer(flac_stats$TD))),]
+    flac_stats$TD <- as.integer(flac_stats$TD)
+    flac_stats$Date <- as.Date(flac_stats$Date)
+    month_td <-flac_stats %>%
+      mutate(month = format(Date, "%m"), year = format(Date, "%Y")) %>%
+      group_by(month, year) %>%
+      summarise(total = sum(TD))
+    month_td <- month_td[which(!is.na(month_td$month)),]
+    month_td$Date <- paste(month_td$year, month_td$month, sep="-")
+    
+    output$flaccoMeter <- renderPlot({
+      ggplot() + geom_line(aes(x=flacco_search$Month, y=flacco_search$Searches, group = 1)) + 
+        geom_point(aes(x=month_td$Date, y=month_td$total))
+    })
     
     output$gamePlot <- renderPlot({
       df <- df[which(df$Minute == input$minutes), ]
